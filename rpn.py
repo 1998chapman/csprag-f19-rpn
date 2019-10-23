b58e0882a25924683e99a44de092744b0075a170
Nathan Chapman <chapmann@umich.edu>
2019-10-23 14:37:08 -0400

    Added functionality to rpn and test_rpn

--- rpn.py
+++ rpn.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
 
 def calculate(string):
-    pass
+    stack = list()
+    for token in string.split():
+        if token == '+':
+            arg1 = stack.pop()
+            arg2 = stack.pop()
+            result = arg1 + arg1
+            stack.append(result)
+        elif token == '-':
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = arg1 - arg2
+            stack.append(result)
+        else:
+            stack.append(int(token))
+    if len(stack) != 1:
+        raise TypeError('Malformed input: ' + string)
+    return stack.pop()
 
 def main():
     while True:
