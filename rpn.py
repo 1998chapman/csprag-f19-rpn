c4e5a6896a4c809fd54a2b87df6abbf3e112cbc3
Nathan Chapman <chapmann@umich.edu>
2019-11-03 14:57:58 -0500

    Added general functionality

--- rpn.py
+++ rpn.py
@@ -16,13 +16,17 @@ def calculate(string):
             value = int(token)
             stack.append(value)
         except ValueError:
-            function = operators[token]
+            try:
+                function = operator.__dir__[token]
+            except KeyError:
+                function = operators[token]
             arg2 = stack.pop()
             arg1 = stack.pop()
             result = function(arg1, arg2)
             stack.append(result)
     if len(stack) != 1:
         raise TypeError('Malformed input: ' + string)
+    print(stack[0])
     return stack.pop()
 
 def main():
